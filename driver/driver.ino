f186f03ad586033ace1949a80f948e04835b31eb
Justin Shetty <jas0504@gmail.com>
2016-11-11 12:25:47 -0500

    Addressed isssues brough up at 11 November meeting (uniqueness check for x.inds and distribution of x.spx)

--- driver/driver.ino
+++ driver/driver.ino
@@ -1,8 +1,9 @@
-#include "generate_signal.h"
+#include "generate_signal.h" //including sig_struct and the function generate_signal
+
 void setup(){
   Serial.begin(9600);
-	sig_struct x;
-  x = generate_signal(10, 2, 1.1);
+	sig_struct x; //initializing the sig_struct
+  x = generate_signal(10, 2, 1.1); //generating a signal with arbitrary parameters for testing
   
   //printing the values returned by generate_signal();
   Serial.print("   Indices: "); //intentionally pre-spaced the word so it will align with Amplitudes
--- driver/generate_signal.h
+++ driver/generate_signal.h
@@ -1,7 +1,7 @@
 struct sig_struct { //defining a structure to hold the generated signal's data
-  int inds[];
-  double spx[];
-  double nu;
+  int inds[]; //indices
+  double spx[]; //spectrum of x
+  double nu; //noise
 };
 
 sig_struct generate_signal(int sigsize, int sparsity, double noise){ //function generate_signal analagous to the Matlab namesake
@@ -9,8 +9,18 @@ sig_struct generate_signal(int sigsize, int sparsity, double noise){ //function 
   randomSeed(analogRead(0)); //setting the random num generators seed based on the analogRead of a disconnected pin
 
   for(int i = 0 ; i < sparsity ; i++){ //iterating 'sparsity' number of times
-    x.inds[i] = random(sigsize); //the ith element of inds is loaded by random() with upper bound sigsize
-    x.spx[i] = random(sigsize); //the ith eleemnt of spx is loaded by random() with upper bound sigsize
+    double a = (double) random(0,sigsize); //random double on the interval (0,sigsize)
+    bool contains;
+    for(int j = 0 ; j < i ; j++){
+      if(x.inds[i] == a){
+        contains = true; //if a appears in x.inds, contains indicates that it should not be included
+      }
+    }
+    if(!contains){
+       x.inds[i] = a; //if x.inds does not contain a, x.inds[i] = a (uniqueness check)
+    }
+
+    x.spx[i] = (double)random(-1,1); //the ith element of spx is loaded by random() with upper bound sigsize
   }
   x.nu = noise; //loading nu to the passed value of noise
   return x; //return the loaded structure
