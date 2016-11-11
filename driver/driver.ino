4994ff36cb3826324226f5b6b309b024ae7999cd
Justin Shetty <jas0504@gmail.com>
2016-11-11 01:52:35 -0500

    Fleshed out test code for generate_signal -- awaiting board to test with

--- driver/driver.ino
+++ driver/driver.ino
@@ -1,7 +1,26 @@
 #include "generate_signal.h"
 void setup(){
+  Serial.begin(9600);
 	sig_struct x;
   x = generate_signal(10, 2, 1.1);
+  
+  //printing the values returned by generate_signal();
+  Serial.print("   Indices: "); //intentionally pre-spaced the word so it will align with Amplitudes
+  for(int i = 0 ; i < x.inds.length ; i++){
+    Serial.print(x.inds[i]);
+    Serial.print(" ");
+  }
+  Serial.print('\n');
+  
+  Serial.print("Amplitudes: ");
+  for(int i = 0 ; i < x.spx.length ; i++){
+    Serial.print(x.spx[i]);
+    Serial.print(" ");
+  }
+  Serial.print('\n');
+
+  Serial.print("Noise: ");
+  Serial.println(x.nu);
 }
 void loop(){
   
