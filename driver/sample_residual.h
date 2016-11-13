69755935d90f864681c8ac9fcc67be03b3fe53b1
Justin Shetty <jas0504@gmail.com>
2016-11-12 23:18:10 -0500

    Changed sample_residual call format
    I'm not sure how much of a difference this will make, but its easier to read

--- driver/sample_residual.h
+++ driver/sample_residual.h
@@ -2,7 +2,7 @@ struct Lambda{
 
 };
 // calling sample_residual
-// sample_residual(&r, &samples, &Lambda, &t, &sig, N)
-void sample_residual(double *r, double *samples, int lenSamples, double *t, double *sig, int N){
+// sample_residual(r, samples, Lambda, t, sig, N)
+void sample_residual(double &r, double &samples, int lenSamples, double &t, double &sig, int N){
   
 }
