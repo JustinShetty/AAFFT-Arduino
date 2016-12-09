4cfdf54ff82a091aa24c804f8f169e4d4415bd46
Justin Shetty <jas0504@gmail.com>
2016-12-09 10:38:45 -0500

    Updated indexing on internal for loops
    internal loops now begin indexing at 0 rather than 1 (may solve issue that code had)

--- driver/generate_tspairs.h
+++ driver/generate_tspairs.h
@@ -19,14 +19,14 @@ void generate_tspairs(tspair ats1[], tspair ats2[], int N, int reps1, int reps2,
 		int r = (int) pow(2,alpha-1)*randomDecimal + 1;
 		int s = 2*r - 1; //s is a random odd integer on the interval [1,N-1]
 		
-		for(int n = 1 ; n < reps2 ; n++){
+		for(int n = 0 ; n < reps2 ; n++){
 			double randomDecimal = (double) random(1000)/1000;
 			int t =  (int) N*randomDecimal;
       tspair temp = {t,s};
 			ats1[(j*reps2)+n] = temp;
 		}
 		
-		for(int n = 1 ; n < reps3 ; n++){
+		for(int n = 0 ; n < reps3 ; n++){
       double randomDecimal = (double) random(1000)/1000;
 			int r = (int) pow(2,alpha-1)*randomDecimal + 1;
       int s = 2*r - 1; //s is a random odd integer on the interval [1,N-1]
