76dd1e3f308c60169eefe3ad9ad3cc326adcd1da
Justin Shetty <jas0504@gmail.com>
2017-03-16 00:31:57 -0400

    test and test2 were not meant to be in the previous commit

--- driver/driver.ino
+++ driver/driver.ino
@@ -12,7 +12,7 @@ void setup() {
   double nu = 0.01;
 
   sig_struct x;
-  generate_signal(x,N,m,nu);
+  //generate_signal(x,N,m,nu);
 
   int input_type = 1;
   int reps1 = 3;
@@ -23,7 +23,7 @@ void setup() {
   //generate the tspairs
   std::vector <tspair> ats1(reps1*reps2);
   std::vector <tspair> ats2(reps1*reps3); 
-  generate_tspairs(ats1, ats2, N, reps1, reps2, reps3);
+  //generate_tspairs(ats1, ats2, N, reps1, reps2, reps3);
   
   Serial.println("wow"); 
   //generate sample set
@@ -31,25 +31,25 @@ void setup() {
   std::vector< std::vector<Complex> > xs2;
   std::vector< std::vector< std::vector<int> > > samp1;
   std::vector< std::vector<int> > samp2;
-  generate_sample_set(xs1, xs2, samp1, samp2, x, N, m, ats1, ats2, width, input_type);
+  //generate_sample_set(xs1, xs2, samp1, samp2, x, N, m, ats1, ats2, width, input_type);
   
   Serial.println("wow");
   //do the sampling
   lam Lambda;
-  fourier_sampling(Lambda, xs1, xs2, m, ats1, ats2, reps1, reps2, reps3, N, width);
+  //fourier_sampling(Lambda, xs1, xs2, m, ats1, ats2, reps1, reps2, reps3, N, width);
   
-  Serial.println("frequency, coefficient");
-  for(int b = 0 ; b < Lambda.freq.size() ; b++){
-    Serial.print(Lambda.freq[b].real(),6);
-    Serial.print(" ");
-    Serial.print(Lambda.freq[b].imag(),6);
-    Serial.print("i");
-    Serial.print(", ");
-    Serial.println(Lambda.coef[b].real(),6);
-    Serial.print(" ");
-    Serial.print(Lambda.coef[b].imag(),6);
-    Serial.print("i");
-  }
+//  Serial.println("frequency, coefficient");
+//  for(int b = 0 ; b < Lambda.freq.size() ; b++){
+//    Serial.print(Lambda.freq[b].real(),6);
+//    Serial.print(" ");
+//    Serial.print(Lambda.freq[b].imag(),6);
+//    Serial.print("i");
+//    Serial.print(", ");
+//    Serial.println(Lambda.coef[b].real(),6);
+//    Serial.print(" ");
+//    Serial.print(Lambda.coef[b].imag(),6);
+//    Serial.print("i");
+//  }
 }
 
 void loop() {
